8010bcf82bba10281e11d78f1d72c4af5cf55297
Young Lecturer <younglecturer7@gmail.com>
2025-08-25 09:51:22 +0100

    correct dashboard code

--- app/encrypter/page.tsx
+++ app/encrypter/page.tsx
@@ -3,7 +3,7 @@ import { EncrypterCard } from '@/components/custom-ui/encrypter-page';
 
 function EncrypterPage() {
   return (
-    <div className="bg-muted flex min-h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex w-full min-h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
       <EncrypterCard />
     </div>
   );
--- components/section-cards.tsx
+++ components/section-cards.tsx
@@ -14,138 +14,144 @@ import Link from "next/link";
 
 export function SectionCards() {
   return (
-    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Encryption Simulator App</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,280{" "}
-            <small className="text-xs text-green-700 italic">
-              successful test
-            </small>
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +99.4%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col justify-between space-y-5 text-sm">
-          <div className="flex-row items-start space-y-2 text-sm w-full">
-            <div className="line-clamp-1 flex gap-2 font-medium text-xs">
-              Message compressed is 100% accurate.
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 text-center gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+     
+        <Card className="w-full min-w-xs">
+          <CardHeader>
+            <CardDescription>Encryption Simulator App</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              1,280{" "}
+              <small className="text-xs text-green-700 italic">
+                successful test
+              </small>
+            </CardTitle>
+            <CardAction>
+              <Badge variant="outline">
+                <IconTrendingUp />
+                +99.4%
+              </Badge>
+            </CardAction>
+          </CardHeader>
+          <CardFooter className="flex-col justify-between space-y-5 text-sm">
+            <div className="flex-row items-start space-y-2 text-sm w-full">
+              <div className="line-clamp-1 flex gap-2 font-medium text-xs">
+                Message compressed is 100% accurate.
+              </div>
+              <div className="text-muted-foreground text-xs italic">
+                15 Visitors for the last 6 months...
+              </div>
             </div>
-            <div className="text-muted-foreground text-xs italic">
-              15 Visitors for the last 6 months...
-            </div>
-          </div>
-          <CardAction className="flex-1 justify-center text-center gap-1.5">
-            <Button variant="outline" size="sm" asChild>
-              <Link href="encrypter-simulator">Open Encryption Simulator</Link>
-            </Button>
-          </CardAction>
-        </CardFooter>
-      </Card>
+            <CardAction className="flex-1 justify-center text-center gap-1.5">
+              <Button variant="outline" size="sm" asChild>
+                <Link href="encrypter-simulator">
+                  Open Encryption Simulator
+                </Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
 
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Decryption Simulator App</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,980{" "}
-            <small className="text-xs text-green-700 italic">
-              successful test
-            </small>
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +99.8%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col justify-between space-y-5 text-sm">
-          <div className="flex-row items-start space-y-2 text-sm w-full">
-            <div className="line-clamp-1 flex gap-2 font-medium text-xs">
-              Message decompressed is 100% accurate.
+        <Card className="w-full min-w-xs">
+          <CardHeader>
+            <CardDescription>Decryption Simulator App</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              1,980{" "}
+              <small className="text-xs text-green-700 italic">
+                successful test
+              </small>
+            </CardTitle>
+            <CardAction>
+              <Badge variant="outline">
+                <IconTrendingUp />
+                +99.8%
+              </Badge>
+            </CardAction>
+          </CardHeader>
+          <CardFooter className="flex-col justify-between space-y-5 text-sm">
+            <div className="flex-row items-start space-y-2 text-sm w-full">
+              <div className="line-clamp-1 flex gap-2 font-medium text-xs">
+                Message decompressed is 100% accurate.
+              </div>
+              <div className="text-muted-foreground text-xs italic">
+                18 Visitors for the last 6 months...
+              </div>
             </div>
-            <div className="text-muted-foreground text-xs italic">
-              18 Visitors for the last 6 months...
-            </div>
-          </div>
-          <CardAction className="flex-1 justify-center text-center gap-1.5">
-            <Button variant="outline" size="sm" asChild>
-              <Link href="decrypter-simulator">Open Decryption Simulator</Link>
-            </Button>
-          </CardAction>
-        </CardFooter>
-      </Card>
+            <CardAction className="flex-1 justify-center text-center gap-1.5">
+              <Button variant="outline" size="sm" asChild>
+                <Link href="decrypter-simulator">
+                  Open Decryption Simulator
+                </Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
 
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Encryption App</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,645{" "}
-            <small className="text-xs text-green-700 italic">
-              successful test
-            </small>
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +99.4%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col justify-between space-y-5 text-sm">
-          <div className="flex-row items-start space-y-2 text-sm w-full">
-            <div className="line-clamp-1 flex gap-2 font-medium text-xs">
-              Message compressed is 100% accurate.
+        <Card className="w-full min-w-xs">
+          <CardHeader>
+            <CardDescription>Encryption App</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              1,645{" "}
+              <small className="text-xs text-green-700 italic">
+                successful test
+              </small>
+            </CardTitle>
+            <CardAction>
+              <Badge variant="outline">
+                <IconTrendingUp />
+                +99.4%
+              </Badge>
+            </CardAction>
+          </CardHeader>
+          <CardFooter className="flex-col justify-between space-y-5 text-sm">
+            <div className="flex-row items-start space-y-2 text-sm w-full">
+              <div className="line-clamp-1 flex gap-2 font-medium text-xs">
+                Message compressed is 100% accurate.
+              </div>
+              <div className="text-muted-foreground text-xs italic">
+                19 Visitors for the last 6 months...
+              </div>
             </div>
-            <div className="text-muted-foreground text-xs italic">
-              19 Visitors for the last 6 months...
-            </div>
-          </div>
-          <CardAction className="flex-1 justify-center text-center gap-1.5">
-            <Button variant="outline" size="sm" asChild>
-              <Link href="encrypter">Open Encryption App</Link>
-            </Button>
-          </CardAction>
-        </CardFooter>
-      </Card>
+            <CardAction className="flex-1 justify-center text-center gap-1.5">
+              <Button variant="outline" size="sm" asChild>
+                <Link href="encrypter">Open Encryption App</Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
 
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Decryption App</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            1,370{" "}
-            <small className="text-xs text-green-700 italic">
-              successful test
-            </small>
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <IconTrendingUp />
-              +99.8%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col justify-between space-y-5 text-sm">
-          <div className="flex-row items-start space-y-2 text-sm w-full">
-            <div className="line-clamp-1 flex gap-2 font-medium text-xs">
-              Message decompressed is 100% accurate.
+        <Card className="w-full min-w-xs">
+          <CardHeader>
+            <CardDescription>Decryption App</CardDescription>
+            <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+              1,370{" "}
+              <small className="text-xs text-green-700 italic">
+                successful test
+              </small>
+            </CardTitle>
+            <CardAction>
+              <Badge variant="outline">
+                <IconTrendingUp />
+                +99.8%
+              </Badge>
+            </CardAction>
+          </CardHeader>
+          <CardFooter className="flex-col justify-between space-y-5 text-sm">
+            <div className="flex-row items-start space-y-2 text-sm w-full">
+              <div className="line-clamp-1 flex gap-2 font-medium text-xs">
+                Message decompressed is 100% accurate.
+              </div>
+              <div className="text-muted-foreground text-xs italic">
+                20 Visitors for the last 6 months...
+              </div>
             </div>
-            <div className="text-muted-foreground text-xs italic">
-              20 Visitors for the last 6 months...
-            </div>
-          </div>
-          <CardAction className="flex-1 justify-center text-center gap-1.5">
-            <Button variant="outline" size="sm" asChild>
-              <Link href="decrypter">Open Decryption App</Link>
-            </Button>
-          </CardAction>
-        </CardFooter>
-      </Card>
+            <CardAction className="flex-1 justify-center text-center gap-1.5">
+              <Button variant="outline" size="sm" asChild>
+                <Link href="decrypter">Open Decryption App</Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
+   
     </div>
   );
 }
